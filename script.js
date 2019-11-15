5047b42e04e2e117ee0fde33d813fbbaf5a18480
sqwezzy <maks.subbotko46@gmail.com>
2019-11-15 23:06:33 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const btnAdd = document.getElementById('btnAdd');
+const nameEvent = document.getElementById('nameEvent');
+const discription = document.getElementById('discription');
+
+btnAdd.addEventListener('click', addInformation);
+
+
+function addInformation() {
+
+}
+
+
+const month = [
+    [1, 8, 15, 22, 29],[2, 9, 16, 23, 30],
+];
